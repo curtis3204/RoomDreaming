64acfe36e9b35bc884494966a2e701b9cb1e35f2
curtis3204 <curtis3204@gmail.com>
2023-06-26 20:54:06 +0800

    0626 20:54

--- UI.py
+++ UI.py
@@ -7,11 +7,13 @@ import numpy as np
 
 
 
-def Generate(Roomtype, UserNumber, RoomImage, ResultController):
-    selectnumber = int(ResultController)
+def Generate(Roomtype, UserNumber, RoomImage, Generatenumber, Displaynumber):
     #read all images in the file
-    allimgspath, selectimgspath = ReadallImages("RoomImages", selectnumber)
-    text = "Room Type: " + Roomtype + "\n" + "Number of Users: " + str(UserNumber)  + "\n" + "Result Number Controller: " + str(ResultController)    
+    allimgspath, selectimgspath = ReadallImages("RoomImages", Generatenumber)
+    text = "Room Type: " + Roomtype + "\n" \
+    + "Number of Users: " + str(UserNumber)  + "\n" \
+    + "Generated Result Number : " + str(Generatenumber) + "\n" \
+    + "Displayed Result Number : " + str(Displaynumber)  
     
     outputs = [text]+selectimgspath
     
@@ -37,6 +39,42 @@ def ReadallImages(path, SelectNumber):
 
 
 
+def GenerateOptions(ResultImgBlock, likebtns, dislikebtns):
+    with gr.Group() as group:
+        with gr.Column():
+            img = gr.outputs.Image(type= "filepath", label="Result Image")
+            with gr.Row():
+                Like = gr.inputs.Checkbox(label="Like")
+                Dislike = gr.inputs.Checkbox(label="Disike")
+            ResultImgBlock.append(img)
+            likebtns.append(Like)
+            dislikebtns.append(Dislike)
+    return group
+
+
+
+
+def GenerateLayout(Total = 50, rownum=4):
+    ResultImgBlock = []
+    likebtns = []
+    dislikebtns = []
+    Groups = []
+
+    Total = int(Total)
+    rownum = int(rownum)
+
+    for i in range(Total//rownum):
+        with gr.Row():
+            for i in range(rownum):
+                group = GenerateOptions(ResultImgBlock, likebtns, dislikebtns)
+                Groups.append(group)
+    if Total % rownum != 0:
+        with gr.Row():
+            for i in range(Total % rownum):
+                group = GenerateOptions(ResultImgBlock, likebtns, dislikebtns)
+                Groups.append(group)
+    return ResultImgBlock, likebtns, dislikebtns, Groups
+
 
 
 
@@ -60,6 +98,12 @@ with gr.Blocks() as demo:
                 Seed = gr.inputs.Slider(minimum=1, maximum=10, step=1, default=1, label="Seed")
                 CN_bool = gr.inputs.Checkbox(label="ControlNet")
         
+        with gr.Row():
+                    gr.Markdown("Result Number & Display Control!!")
+                    GenerateNum = gr.Slider(minimum=1, maximum=50, step=1, default=50, label="Result Generate Number Controller")
+                    DisplayNum = gr.Slider(minimum=1, maximum=50, step=1, default=50, label="Result Display Number Controller")
+                    ConfirmDisplay = gr.Button("Confirm Display Number")
+        
         with gr.Accordion("Open for More!"):
             gr.Markdown("Look at me...")
 
@@ -76,63 +120,68 @@ with gr.Blocks() as demo:
             with gr.Column():
                 gr.Markdown("Room Image")
                 RoomImage = gr.inputs.Image(shape=(224, 224), label="Room Image")
-                with gr.Row():
-                    gr.Markdown("Result Number Control!!")
-                    ResultController = gr.Slider(minimum=1, maximum=50, step=1, default=50, label="Result Number Controller")
-                    Confirm = gr.Button("Confirm")
-                GenerateButton = gr.Button("Start Iteration")
+                
+
+                StartIteration = gr.Button("Start Iteration")
                 
         
         gr.Markdown("## Iteration Results")
-        Banner = gr.outputs.Textbox(label="Output")
+        Banner = gr.outputs.Textbox(label="System Log")
         
         with gr.Row():
             NextIteration = gr.Button("Next Iteration")
+            ClearIteration = gr.Button("Clear Iteration")
             StopIteration = gr.Button("Finish!")
+            
+
+        #Tab Number
+        TabNum = 1
+        Tabs = []
+        ResultImgBlock, likebtns, dislikebtns, groups = [], [], [], []
 
 
-        ResultImgBlock = []
-        likebtns = []
-        dislikebtns = []
+        def generateTab(Tabs, TabNum, ResultImgBlock, likebtns, dislikebtns, groups):
+            with gr.Tab("Iteration "+str(TabNum)) as tab:
+                ResultImgBlock, likebtns, dislikebtns , groups= GenerateLayout(50, 4)
+            print(tab)
+            TabNum += 1
+            Tabs.append(tab)
+            return TabNum , ResultImgBlock, likebtns, dislikebtns, groups
 
-        rownum = 4
-
-        def GenerateLayout(resultnumber):
-            for i in range(resultnumber//rownum):
-                with gr.Row():
-                    for i in range(rownum):
-                        with gr.Column():
-                            img = gr.outputs.Image(type= "filepath", label="Result Image")
-                            with gr.Row():
-                                Like = gr.inputs.Checkbox(label="Like")
-                                Dislike = gr.inputs.Checkbox(label="Disike")
-                            ResultImgBlock.append(img)
-                            likebtns.append(Like)
-                            dislikebtns.append(Dislike)
-            if resultnumber % rownum != 0:
-                with gr.Row():
-                    for i in range(resultnumber % rownum):
-                        with gr.Column():
-                            img = gr.outputs.Image(type= "filepath", label="Result Image")
-                            with gr.Row():
-                                Like = gr.inputs.Checkbox(label="Like")
-                                Dislike = gr.inputs.Checkbox(label="Disike")
-                            ResultImgBlock.append(img)
-                            likebtns.append(Like)
-                            dislikebtns.append(Dislike)
-            return ResultImgBlock
-
-        GenerateLayout(50)        
-        # ResultController.change(GenerateLayout, ResultController, resultimgs)
+           
+        TabNum, ResultImgBlock, likebtns, dislikebtns, groups = generateTab(Tabs, TabNum, ResultImgBlock, likebtns, dislikebtns, groups)
         
 
-        GenerateButton.click(fn = Generate, inputs=[Roomtype, UserNumber, RoomImage, ResultController], outputs=[Banner]+ResultImgBlock)
-        
-        
+        a,b,c,d = [], [], [], []
+        e, f, g, h = [], [], [], []
+        generateTab(Tabs, TabNum, a, b, c, d)
+        generateTab(Tabs, TabNum, e, f, g, h)
 
+        # 0 ,1 ,2, 3, 4, 5
+        
+        def DisplayImg(DisplayNum):
+            DisplayNum = int(DisplayNum)
+            return {ResultImgBlock[i]: gr.update(visible=False) for i in range(len(ResultImgBlock)-1, DisplayNum-1, -1)}
+    
+        def Displaylikebtns(DisplayNum):
+            DisplayNum = int(DisplayNum)
+            return {likebtns[i]: gr.update(visible=False) for i in range(len(likebtns)-1, DisplayNum-1, -1)} 
+
+        def Displaydislikebtns(DisplayNum):
+            DisplayNum = int(DisplayNum)
+            return {dislikebtns[i]: gr.update(visible=False) for i in range(len(dislikebtns)-1, DisplayNum-1, -1)}  
+                
+        def Display(DisplayNum):
+            DisplayNum = int(DisplayNum)
+            return {**DisplayImg(DisplayNum), **Displaylikebtns(DisplayNum), **Displaydislikebtns(DisplayNum)}
+
+
+        ConfirmDisplay.click(fn = Display, inputs=[DisplayNum], outputs=ResultImgBlock+likebtns+dislikebtns)
+
+        StartIteration.click(fn = Generate, inputs=[Roomtype, UserNumber, RoomImage, GenerateNum, DisplayNum], outputs=[Banner]+ResultImgBlock)
         
         
 
 
 if __name__ == "__main__":
-    demo.launch()
+    demo.launch(share=True)
